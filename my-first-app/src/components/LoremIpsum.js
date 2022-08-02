6a9b3382df86e9e331e2fb28fdcd4c1da176e963
Thito Ribeiro <thito.ribeiro@ppro.com>
2022-08-01 21:15:07 -0300

    React basic component

---
+++ my-first-app/src/components/LoremIpsum.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoremIpsum() {
+    return <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>;
+  }
+
+  export default LoremIpsum;
---
+++ my-first-app/src/components/planets/index.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import Planet from "./planet";
+
+const Planets = () => {
+    return (
+        <Fragment>
+            <h3>Planets List</h3>
+            <hr/>
+            <Planet/>
+        </Fragment>
+    )
+}
+
+export default Planets;
